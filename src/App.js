35a4da7214db7583becacbae0963d64d3a8d142e
ralphmaronilla <ralphmaronilla003@gmail.com>
2022-03-28 12:12:37 +0800

    react moviecouch

---
+++ src/App.js
@@ -0,0 +1,59 @@
+import {useState,useEffect} from 'react'
+
+import './App.css'
+import MovieCard from './MovieCard';
+import SearchIcon from './search.svg';
+
+
+const API_URL = 'http://omdbapi.com?apikey=fafc8352';
+
+const App = () => {
+    const [movies,setMovies] = useState([])
+    const [searchTerm,setSearchTerm] = useState('');
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`)
+        const data = await response.json()
+        setMovies(data.Search)
+    }
+  
+    useEffect(() => {
+        searchMovies('spiderman')
+    },[])
+    return(
+      <div className='app'>
+          <h1>MovieCouch</h1>
+          <div className='search'>
+              <input
+                placeholder='Search for movies'
+                value={searchTerm}
+                onChange = {(e) => setSearchTerm(e.target.value)}
+              />
+              <img src={SearchIcon}
+              alt ="search"
+              onClick={() => searchMovies(searchTerm)} />
+          </div>
+          {
+              movies?.length > 0 
+              ?(
+                <div className='container'>
+                    {
+                        movies.map((movie) => (
+                            <MovieCard 
+                            key={movie.imdbID}
+                            movie={movie} />
+                        ))
+                    }
+                
+               </div>
+              ) : (
+                  <div className='empty'>
+                       <h2>No Movies Found</h2>
+                  </div>
+              )
+          }
+         
+      </div>
+    )
+}
+
+export default App;
